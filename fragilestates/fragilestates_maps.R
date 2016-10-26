f7c6d3029ea640ba8efb436511ca9c47e3b77517
flaneuse <laura.d.hughes@gmail.com>
2016-10-26 12:14:29 -0400

    prelim maps

---
+++ fragilestates/fragilestates_maps.R
@@ -0,0 +1,133 @@
+# Fragile States Index visualization -----------------------------------------
+#
+# fragilestates_overlap.R: script to visualize three classifications of fragile states
+#
+# Geo data from Natural Earth: http://www.naturalearthdata.com/downloads/10m-cultural-vectors/
+#
+# Laura Hughes, lhughes@usaid.gov, 6 October 2016
+# with Aaron Roesch (aroesch@usaid.gov) and Nada Petrovic (npetrovic@usaid.gov)
+#
+# Copyright 2016 by Laura Hughes via MIT License
+
+# Maps to show, for most recent data:
+# * USAID fragility score
+# * Any crisis in the past 5 years
+# * major conflict (battle deaths > 1000)
+# * major violent extremism (terrorism > 500 deaths)
+# * attempted coup
+# * adverse regime change
+# * mass atrocity
+
+
+# libraries to load -------------------------------------------------------
+pkgs = c('llamar', 'dplyr', 'tidyr', 'frontier', 'RColorBrewer', 'extrafont', 'readxl')
+
+# Install and load packages -----------------------------------------------
+
+# Check if packages are installed
+alreadyInstalled = installed.packages()[, "Package"]
+
+toInstall = pkgs[!pkgs %in% alreadyInstalled]
+
+# Install anything that isn't already installed.
+if (length(toInstall > 0)) {
+  print(paste0("Installing these packages: ", paste0(toInstall, collapse = ", ")))
+  
+  install.packages(toInstall)
+}
+
+
+# Load packages
+for (i in seq_along(pkgs)) {
+  library(pkgs[i], character.only = TRUE, quietly = TRUE)
+}
+
+loadfonts(quiet = TRUE)
+
+
+
+# import data -------------------------------------------------------------
+
+# fragile scores and threats
+df = read_excel('~/Documents/USAID/mini projects/Fragile States - (Aaron Roesch)/SBU_fragility_threats.xlsx')
+
+df2015 = df %>% 
+  filter(year == 2015) 
+
+
+# import geo data ---------------------------------------------------------
+
+# import spatial data
+library(frontier)
+library(maptools)
+
+geo = frontier::shp2df(baseDir = '~/Documents/USAID/geodata/ne_10m_admin_0_countries/',
+                 layerName = 'ne_10m_admin_0_countries', getCentroids = FALSE)
+
+# find which code contains the correct iso code. options:
+# ADM0_A3_US
+# ADM0_A3_IS
+# ADM0_A3_UN
+# ADM0_A3_WB
+# WB_A3
+# UN_A3
+# ISO_A3
+# BRK_A3
+# GU_A3
+# SU_A3
+# SOV_A3
+# ADM0_A3
+
+# Trying first WB, assuming World Bank
+all(geo$ADM0_A3_WB == geo$WB_A3)
+all(geo$ADM0_A3_UN == geo$UN_A3)
+
+codes = data.frame(code = unique(geo$WB_A3),
+                 in_ne = TRUE)
+
+# test merge
+x = full_join(df, codes, by = c("code"))
+
+View(x %>% filter(is.na(in_ne)))
+
+# merges everything, aside from Norway-- which is -99 in WB_A3
+
+# Fix Norway
+geo = geo %>% 
+  mutate(code = ifelse(NAME == 'Norway', 'NOR', as.character(WB_A3)))
+
+codes = data.frame(code = unique(geo$code),
+                   in_ne = TRUE)
+# test merge
+x = full_join(df, codes, by = c("code"))
+
+View(x %>% filter(is.na(in_ne)))
+
+
+# Filter out extraneous geo data ------------------------------------------
+
+geo = geo %>% 
+  filter(NAME != 'Antarctica') %>% 
+  select(code, lat, long, group, order, id, REGION_WB, CONTINENT)
+
+# merge fragile data w/ geo data ------------------------------------------
+df_geo = full_join(df2015, geo, by = c("code"))
+
+
+# choropleth --------------------------------------------------------------
+
+max_val = max(abs(range(df2015$al, na.rm = TRUE)))
+
+p = frontier::plot_map(df_geo, fill_var = 'al') +
+  scale_fill_gradientn(colours = rev(brewer.pal(10, 'RdYlBu')), 
+                       limits = c(-max_val, max_val),
+                       na.value = grey15K)
+
+save_plot('~/Documents/USAID/mini projects/Fragile States - (Aaron Roesch)/al_score.pdf',
+          width = 7, height = 4)
+
+p = frontier::plot_map(df_geo, fill_var = 'factor(any_last5)') +
+  scale_fill_manual(values = c('0' = grey15K, '1' = brewer.pal(11, 'Spectral')[2]))
+
+save_plot('~/Documents/USAID/mini projects/Fragile States - (Aaron Roesch)/any_events_5y.pdf',
+          width = 7, height = 4)
