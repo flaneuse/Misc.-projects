ebc15d1d3fc5dc42afd0251fb7e737fdcccbc5d5
flaneuse <laura.d.hughes@gmail.com>
2016-10-28 15:39:52 -0400

    init graph

---
+++ transition_hiringmech.R
@@ -0,0 +1,33 @@
+library(readxl)
+library(waffle)
+library(dplyr)
+library(tidyr)
+library(forcats)
+library(extrafont)
+library(stringr)
+library(llamar)
+
+loadfonts()
+df = read_excel('~/Documents/USAID/mini projects/Tranistion team HCTM - (Nick Vivio)/HCTM HR Flat File.xlsx', skip = 3)
+
+df = df %>% 
+  filter(`Row Labels` == 'Grand Total') %>% 
+  select(-`0`) %>% 
+  gather(hiring_mech, n, -`Row Labels`) %>% 
+  mutate(mech = str_replace_all(hiring_mech, 'Sum of ', '')) %>% 
+  arrange(desc(n))
+
+ggplot(df, aes(x = fct_reorder(hiring_mech, n), y = n)) +
+  geom_point()
+
+df$hiring_mech = factor(df$hiring_mech,
+                        levels = df$hiring_mech)
+
+waffle(df$n/10,  use_glyph = 'user', glyph_size = 7, rows = 30) + theme(legend.position = 'none') + 
+  geom_text(aes(x = 10, y = 1, label = paste0(mech, ': ', n)), 
+            data = df, size = 7, family = 'Lato Light')
+
+
+ggsave('~/Documents/USAID/mini projects/Tranistion team HCTM - (Nick Vivio)/hctm_isotype.svg', 
+       width = 9, height = 10,
+       units = 'in')
